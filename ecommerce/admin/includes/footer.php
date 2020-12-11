acc3fe39eb695807bb697e99982663f40a5fff15
Icaro Quintão <icarobicalho@hotmail.com>
2020-12-11 13:43:37 -0300

    update

--- ecommerce/admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Todos os direitos reservados.</b>
-    </div>
-    <strong>Copyright &copy; 2020 <a href="">Icaro-Projects</a></strong>
-</footer>
--- ecommerce/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>Todos os direitos reservados.</b>
-      </div>
-      <strong>Copyright &copy; 2020 Brought to You By <a href="https://www.linkedin.com/in/icaroquintao/">icaroquintao</a></strong>
-    </div>
-</footer>
